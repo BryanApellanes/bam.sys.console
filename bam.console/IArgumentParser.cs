a7ef87554c32b34a317d3a8dfec1202db03eb8af
Bryan Apellanes <bryan.apellanes@gmail.com>
2024-10-29 21:15:35 -0500

    wip

--- bam.console/IArgumentParser.cs
+++ bam.console/IArgumentParser.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Bam.Console
 {
+    /// <summary>
+    /// An interface defining a method for parsing command line arguments.
+    /// </summary>
     public interface IArgumentParser
     {
         IParsedArguments ParseArguments(string[] arguments); 
